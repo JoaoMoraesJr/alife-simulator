7264ffd1f57a038b95eea82ed8d7d56b213a6ebe
JoaoMoraesJr <joao_junior1997@hotmail.com>
2018-10-15 21:19:10 -0300

    Simulation base

---
+++ ALife Simulator/Assets/EnergyControll.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyControll : MonoBehaviour {
+
+    [SerializeField]
+    private float energy = 0f;
+
+    [SerializeField]
+    private float startEnergy = 10f;
+    [SerializeField]
+    private float energyCapacity = 50f;
+    [SerializeField]
+    private float energyLose = 2f;
+
+    private void Start()
+    {
+        energy = startEnergy;
+    }
+    public void gainEnergy (float gain)
+    {
+        energy += gain;
+
+        if (energy > energyCapacity) energy = energyCapacity;
+    }
+
+    private void Update()
+    {
+        energy -= energyLose * Time.deltaTime;
+
+        float resize = energy * 0.1f;
+        this.gameObject.transform.localScale = new Vector3 (resize, resize, 0);
+
+        if (energy <= 0) Die();
+    }
+
+    private void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ ALife Simulator/Assets/SimulationManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationManager : MonoBehaviour {
+
+    public GameObject foodPrefab;
+    public Transform simulationArea;
+    public int foodAmount = 50;
+
+	// Use this for initialization
+	void Start () {
+        SpawnFood(foodAmount);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnFood(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            var food = Instantiate(foodPrefab);
+            food.transform.position = simulationArea.position + new Vector3(Random.Range(-1f, 1f) * simulationArea.localScale.x/2, Random.Range(-1f, 1f) * simulationArea.localScale.y/2, 0);
+        }
+    }
+}
